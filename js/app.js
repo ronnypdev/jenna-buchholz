7b8cb9752979c1b4c19e21230e05af911e97cc92
Ronny Perez <ronnypdev@gmail.com>
2020-10-04 08:35:49 -0400

    Adding progress bar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const pixelsTag = document.querySelector('.pixels');
+const bodyTag = document.querySelector('body');
+const progressTag = document.querySelector('.progress');
 
 // when we scroll the page, update the pixels tags to be how far we've scrolled
 const pageScroll = () => {
@@ -7,3 +9,16 @@ const pageScroll = () => {
 };
 
 document.addEventListener('scroll', pageScroll, false);
+
+// when we scroll the page, make a progress bar that track of the distance
+const progressBar = () => {
+  const pixels = window.pageYOffset;
+  const pageHeight = bodyTag.getBoundingClientRect().height;
+  const totalScrollableDistance = pageHeight - window.innerHeight;
+
+  const percentage = pixels / totalScrollableDistance;
+
+  progressTag.style.width = `${100 * percentage}%`;
+};
+
+document.addEventListener('scroll', progressBar, false);
